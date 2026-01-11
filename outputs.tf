0e638c712344116a8fac62a03a7f4ca7c080b8f8
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:43:03 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_integration_account_sessions" {
+  description = "All logic_app_integration_account_session resources"
+  value       = azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions
+}
+
