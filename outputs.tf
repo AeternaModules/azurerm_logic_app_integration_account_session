caf6bd1a6b3cf4de8c4e6a86d5a506051188d640
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "logic_app_integration_account_sessions_id" {
   description = "Map of id values across all logic_app_integration_account_sessions, keyed the same as var.logic_app_integration_account_sessions"
-  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.id }
+  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "logic_app_integration_account_sessions_content" {
   description = "Map of content values across all logic_app_integration_account_sessions, keyed the same as var.logic_app_integration_account_sessions"
-  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.content }
+  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.content if v.content != null && length(v.content) > 0 }
 }
 output "logic_app_integration_account_sessions_integration_account_name" {
   description = "Map of integration_account_name values across all logic_app_integration_account_sessions, keyed the same as var.logic_app_integration_account_sessions"
-  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.integration_account_name }
+  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.integration_account_name if v.integration_account_name != null && length(v.integration_account_name) > 0 }
 }
 output "logic_app_integration_account_sessions_name" {
   description = "Map of name values across all logic_app_integration_account_sessions, keyed the same as var.logic_app_integration_account_sessions"
-  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.name }
+  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "logic_app_integration_account_sessions_resource_group_name" {
   description = "Map of resource_group_name values across all logic_app_integration_account_sessions, keyed the same as var.logic_app_integration_account_sessions"
-  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.resource_group_name }
+  value       = { for k, v in azurerm_logic_app_integration_account_session.logic_app_integration_account_sessions : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 
